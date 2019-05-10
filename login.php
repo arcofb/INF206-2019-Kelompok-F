5e11173668d1f697e967a43445979a857cd81ab1
Fitri Mauizah <fitri.mzh@mhs.unsyiah.ac.id>
2019-05-10 12:36:18 +0700

    Masukkan Login

---
+++ login.php
@@ -0,0 +1,91 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+    </head>
+    <body>
+
+        <div class="login-page">
+            <div class="form">
+              <h2>Masuk</h2>
+              <form class="login-for" method="post" action="">
+              <input type="nope" id="nope" name="nope" placeholder="No telepone"/>
+              <input type="password" id="password" name="password" placeholder="Password"/>
+                <button>Masuk</button>
+                <p class="message">Belum punya akun? <a href="">Daftar Sekarang</a> atau <a href="">Beranda</a></p>
+              </form>
+            </div>
+          </div>	
+    
+    
+<style>@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
+.login-page {
+  width: 360px;
+  padding: 8% 0 0;
+  margin: auto;
+}
+.form {
+  position: relative;
+  z-index: 1;
+  background: orange;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 40px;
+  padding-top: 10px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+.form input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
+}
+.form button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.form button:hover,.form button:active,.form button:focus {
+  background: aqua;
+}
+.form .message {
+  margin: 15px 0 0;
+  color: black;
+  font-size: 12px;
+}
+.form .message a {
+  color: aqua;
+  text-decoration: none;
+}
+.register-form {
+  position: relative;
+  z-index: 1;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 40px;
+  padding-top: 10px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}</style>
+        </body>
+</html>
